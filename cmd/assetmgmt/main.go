25e44ee8b75899f3358c93a938fe45c1626f5a2a
Anurag Kumar <anurag.hcu@gmail.com>
2024-02-25 00:15:39 +0530

    added recover

--- cmd/assetmgmt/main.go
+++ cmd/assetmgmt/main.go
@@ -7,18 +7,28 @@ import (
 	"log"
 )
 
-func main() {
-	appid, filePath := commandline.GetCommandLineArguments()
-	log.Println("application ID and filepath : ", appid, filePath)
-	assetprocessor.ProcessAssets(appid, filePath, batchsize)
-}
-
 var batchsize int = 1000 // read from config file
 
 func init() {
 	var err error
 	batchsize, err = utils.GetBatchSize("../../config/config.json")
 	if err != nil {
-		log.Println("error in reading config file ", err)
+		log.Printf("error in reading config file: %v", err)
 	}
+}
+
+func main() {
+	run()
+}
+
+func run() {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Printf("Recovered from panic: %v", r)
+		}
+	}()
+
+	appid, filePath := commandline.GetCommandLineArguments()
+	log.Println("application ID and filepath: ", appid, filePath)
+	assetprocessor.ProcessAssets(appid, filePath, batchsize)
 }
