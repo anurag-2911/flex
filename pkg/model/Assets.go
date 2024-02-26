bda163dfe1e541f2cb7511a6108afbceafd995e7
Anurag Kumar <anurag.hcu@gmail.com>
2024-02-26 19:10:48 +0530

    validation in Assets.go

--- pkg/model/Assets.go
+++ pkg/model/Assets.go
@@ -1,5 +1,11 @@
 package model
 
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
 type Asset struct {
 	ComputerID    string
 	UserID        string
@@ -14,3 +20,17 @@ const DESKTOP string = "desktop"
 type Config struct {
 	BatchSize int `json:"batchsize"`
 }
+func (c *Config) Validate() error {
+	if c.BatchSize <= 0 {
+		return errors.New("invalid batch size: must be greater than zero")
+	}
+	return nil
+ }
+ 
+ func (a *Asset) Validate() error {
+	 validType := strings.EqualFold(a.ComputerType, LAPTOP) || strings.EqualFold(a.ComputerType, DESKTOP)
+	 if !validType {
+		 return fmt.Errorf("invalid computer type: %s", a.ComputerType)
+	 }
+	 return nil
+ }
